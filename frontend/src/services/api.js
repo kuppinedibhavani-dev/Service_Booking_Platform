31fc377414bcda97ffac01f8bce48c561dceda6f
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2026-07-02 20:27:03 +0530

    Updated frontend API for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://service-booking-platform-lakl.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
