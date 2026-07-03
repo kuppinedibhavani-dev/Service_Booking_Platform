d5516ee4c87a1482a3c6b822167dea5eb8f59c8d
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2026-07-03 16:27:37 +0530

    Implemented Razorpay signatur

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -39,6 +39,7 @@ function DashboardPage() {
   await API.post("/payments/verify", {
     razorpay_order_id: response.razorpay_order_id,
     razorpay_payment_id: response.razorpay_payment_id,
+    razorpay_signature: response.razorpay_signature,
     bookingId
   });
 
