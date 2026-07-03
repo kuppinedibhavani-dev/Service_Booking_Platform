aa0bedb4eac6236a59309f2b35f1e64f198d2a62
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2026-07-03 15:25:22 +0530

    Updated transporter

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -10,7 +10,10 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 
 // Send Notification
