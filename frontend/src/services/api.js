5a2931e06ebdaee9510e59aab8884c66bde6793d
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2026-07-03 14:20:44 +0530

    Fixed production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://service-booking-platform-lakl.onrender.com"
+  baseURL: "https://service-booking-platform-lakl.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
